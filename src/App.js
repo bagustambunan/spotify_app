027b219a341b6402bafeb418aadec1c172d44247
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-20 22:45:37 +0700

    Add LoginButton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const LoginButton = () => {
+
+    let client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+    let scope = 'playlist-modify-private';
+    let redirect_uri = 'http://localhost:3000';
+
+    let spotify_url = 'https://accounts.spotify.com/authorize';
+        spotify_url += '?response_type=token';
+        spotify_url += '&client_id=' + encodeURIComponent(client_id);
+        spotify_url += '&scope=' + encodeURIComponent(scope);
+        spotify_url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+    return (
+      <a href={spotify_url} className="bg-spotify_main hover:bg-gray-600 w-60 rounded-full text-white font-medium px-1 py-1 flex cursor-pointer justify-center align-middle">
+        <i className="m-2 fab fa-spotify"></i>
+        <a className="my-1">LOG IN WITH SPOTIFY</a>
+      </a>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginButton/>
+    </>
   );
 }
 
