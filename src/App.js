59bac7e1df321ba22aefdd84cd8ff0004276301b
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-20 22:53:36 +0700

    Add function getHashParams

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
     );
   }
 
+  function getHashParams() {
+    let hashParams = {};
+    let e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
   return (
     <>
       <LoginButton/>
