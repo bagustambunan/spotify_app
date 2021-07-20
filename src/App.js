7a3c075d80162eb5631a0f5c3f1c66c430cff72e
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-20 23:58:12 +0700

    Add Track component

--- src/App.js
+++ src/App.js
@@ -34,14 +34,24 @@ function App() {
         <input
           onChange={(event) => {set_query(event.target.value)}}
           value={query} type="text"
-          className="bg-gray-200 px-2 py-1 rounded-bl rounded-tl w-80 mb-3"
+          className="bg-white px-2 py-1 rounded-bl rounded-tl w-80 mb-3"
           placeholder="Type anything..."></input>
         <button onClick={() => {handleClick()}} className="bg-spotify_main hover:bg-gray-600 px-2 py-1 mb-3 text-white rounded-br rounded-tr"><i className="fa fa-search"></i></button>
       </div>
     );
   }
 
-  
+  const Track = (props) => {
+    return (
+      <div className="bg-spotify_card px-5 py-5 rounded w-1/5 mr-4 mb-4 cursor-pointer hover:bg-spotify_card_hover">
+        <img src={props.image_url} title={props.album_name} alt="{props.album_name}" className="object-cover rounded h-40 w-full"/>
+        <p className="text-base mt-2 mb-1 font-bold text-gray-100">{props.track_title}</p>
+        <div className="">
+          <p className="text-sm text-gray-300">{props.artist_name}</p>
+        </div>
+      </div>
+    );
+  }
 
   function handleClick() {
     try {
@@ -78,10 +88,23 @@ function App() {
   });
 
   return (
-    <>
+    <div className="bg-dark_main min-h-screen">
       <LoginButton/>
       <Form/>
-    </>
+
+      {tracks.map((item) => {
+        return (
+          <Track
+            key={item.album.id}
+            image_url={item.album.images[0].url}
+            track_title={item.name}
+            artist_name={item.album.artists[0].name}
+            album_name={item.album.name}
+          />
+        );
+      })}
+
+    </div>
   );
 }
 
